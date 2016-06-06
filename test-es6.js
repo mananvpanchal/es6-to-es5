948bd953eaa60ea8258fa4fea177665fdea2d56c
Manan V. Panchal <mananvpanchal@gmail.com>
2016-06-06 11:26:13 +0530

    init

---
+++ test-es6.js
@@ -0,0 +1,20 @@
+export default () => {
+	return 'Hello';
+};
+
+export const abc = {
+	testFunc: () => {
+		return 'Hello 1';
+	}
+};
+
+export class Xyz {
+
+	constructor() {
+		
+	}
+
+	sampleFunc() {
+
+	}
+}
